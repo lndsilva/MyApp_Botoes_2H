2ef0167f9f80fa0bb7d0ac15023b59bd631a5f5e
lndsilva <lndsilva@hotmail.com>
2021-10-18 19:34:56 -0300

    splash

---
+++ app/src/main/java/br/com/etecia/myapp/Splash_Activity.java
@@ -0,0 +1,28 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        //Criar um temporizador com uma thread para simular uma janela de splash
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                        MainActivity.class));
+            }
+        }, 3000);
+
+    }
+}
